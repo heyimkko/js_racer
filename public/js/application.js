993c617abff950926c4e3bfba0764e12299769ed
Tyler Kuhn <tyler@wargoats.com>
2013-03-19 12:43:09 -0700

    refactoring js into oo

--- public/js/application.js
+++ public/js/application.js
@@ -1,75 +1,138 @@
-$(document).ready(function() {
-  var date = new Date();
-  var time = date.getTime();
+var Game = function Game($, args) {
+  this.args = args ? args : {};
+  this.players = [];
+  this.start_time;
+  this.track_length = this.args.track_length;
 
-  $('button.restart').hide();
-  
-  function start_game() {
-    date = new Date();
-    time = date.getTime();
-    console.log(time)
+  _this = this;
 
-    $(document).on('keyup', function(event) {
-      switch(event.keyCode) {
-        case 80:
-          update_player_position('player2');
-          break;
-        case 81:
-          update_player_position('player1');
-          break;
-      }
+  this.getTime = function() {
+    var date = new Date();
+    return date.getTime();
+  }
+
+  this.add_players = function(players) {
+    $(players).each(function(key, player) {
+      _this.add_player(player);
     });
   }
 
-  function countdown() {
-    var counter = 3;
-    var count_int = setInterval(function() {
-      $('#counter').text(counter).show();
-      counter--;
-      if (counter < 0) {
-        clearInterval(count_int);
-        $('#counter').hide();
-        start_game();
+  this.add_player = function(player) {
+    console.log(player);
+    var Player = function Player(player) {
+      this.id = player.id;
+      this.key = player.key;
+
+      this.reset = function() {
+        this.place = 0;
       }
-    }, 1000);
+      this.reset();
+    }
+
+    _this.players.push(new Player(player));
   }
 
-  countdown();
+  this.countdown = function(args) {
+    if (_this.players.length > 1) {
+      var args = args ? args : {start: 3};
+      var count_int = setInterval(function() {
+        if (args.interval) {
+          args.interval(args.start);
+        }
+        args.start--;
+        if (args.start < 0) {
+          clearInterval(count_int);
+          if (args.finish) {
+            args.finish();
+          }
+          _this.start();
+        }
+      }, 1000);
+    } else {
+      console.warn("Game countdown initialized without players")
+    }
+  }
+
+  this.start = function() {
+    if (_this.players.length > 1) {
+      _this.add_controls();
+      _this.start_time = _this.getTime();
+    } else {
+      console.warn("Game started without players")
+    }
+  }
+
+  this.add_controls = function() {
+    $(document).on('keyup', function(event) {
+      var player;
+      $(_this.players).each(function(key, player) {
+        if (event.keyCode == player.key) {
+          player.place += 1;
+          if (_this.args.player_move) {
+            _this.args.player_move(player.id + 1);
+          }
+        }
+      });
+
+      _this.check_win();
+    });
+  }
+
+  this.check_win = function() {
+    $(_this.players).each(function(key, player) {
+      if (player.place == _this.track_length -1) {
+        _this.finish(player);
+      }
+    })
+  }
+
+  this.finish = function(player) {
+    _this.remove_controls();
+    if (_this.args.player_win) {
+      var finish_time = _this.getTime();
+      _this.args.player_win(player.id + 1, finish_time - _this.start_time);
+    }
+  }
+
+  this.remove_controls = function() {
+    $(document).unbind('keyup');
+  }
+
+  this.restart = function() {
+    $(_this.players).each(function(key, player) {
+      player.reset();
+    });
+    _this.start();
+  }
+}
+
+$(document).ready(function() {
+  $('button.restart').hide();
+
+  function start_game() {
+    $('#counter').hide();
+  }
+
+  function countdown(n) {
+    $('#counter').text(n).show();
+  }
 
   function update_player_position(player) {
-    $('#' + player + "_strip td.active").removeClass('active').next().addClass('active');
-    checkWinner();
+    $('#player' + player + "_strip td.active").removeClass('active').next().addClass('active');
   }
 
-  function checkWinner() {
-    var $winning_cell = $('td:last-child.active');
-    var $winning_row = $winning_cell.parent('tr');
-    if ($winning_cell) {
-      var player_strip_id = $winning_row.attr('id');
-      if (player_strip_id) {
-        var player_id = player_strip_id.substr(0, player_strip_id.indexOf('_'));
-        $('#winner').addClass(player_id);
+  function win(player, time) {
+    $('#winner').addClass("player" + player);
+    $('#winner').append((time / 1000) + " " + "Seconds!");
 
-
-        $(document).unbind('keyup');
-
-        player_id = $winning_row.attr('data-player');
-        console.log(player_id);
-        date = new Date();
-        time = date.getTime() - time;
-        
-        $('#winner').append((time / 1000) + " " + "Seconds!")
-        
-        $.ajax({
-          url: '/save',
-          type: 'post',
-          data: {time: time, winner: player_id}
-        }).done(function(data, status, xhr) {
-          console.log(data, status, xhr);
-          $('button.restart').show();
-        })
-      }
-    }
+    $.ajax({
+      url: '/save',
+      type: 'post',
+      data: {time: time, winner: player}
+    }).done(function(data, status, xhr) {
+      // console.log(data, status, xhr);
+      $('button.restart').show();
+    });
   }
 
   $('button.restart').click(function(e) {
@@ -78,6 +141,13 @@ $(document).ready(function() {
     $('#winner').empty().removeClass();
     $(this).hide();
 
-    countdown();
+    game.restart();
   });
+
+  var track_length = $('.racer_table tr:first-child td').length;
+  var player1_id = $('#player1_strip').attr('data-player');
+  var player2_id = $('#player2_strip').attr('data-player');
+  var game = new Game($, {track_length: track_length, player_move: update_player_position, player_win: win});
+  game.add_players([{id: player1_id, key: 81}, {id: player2_id, key: 80}]);
+  game.countdown({start: 3, interval: countdown, finish: start_game});
 });
